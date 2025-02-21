87ddb8eebfca04d1d11fd3ea850e878be7f1b7c2
levidowski <leviferreira1677@gmail.com>
2025-02-21 17:53:11 -0300

    Aqui será feita a atividade de Repetição e Matriz

---
+++ src/br/edu/fatecpg/alejava/RepeticaoeMatriz.java
@@ -0,0 +1,10 @@
+package br.edu.fatecpg.alejava;
+
+public class RepeticaoeMatriz {
+
+	public static void main(String[] args) {
+		System.out.println("Aqui será feito a atividade de Repetição e Matriz: ");
+
+	}
+
+}
