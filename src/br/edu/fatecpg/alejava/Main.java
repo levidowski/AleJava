a42e263f8902e420f3085f6cd3758ba014a80dbd
levidowski <leviferreira1677@gmail.com>
2025-02-21 17:31:22 -0300

    primeira modificação commit

--- src/br/edu/fatecpg/alejava/Main.java
+++ src/br/edu/fatecpg/alejava/Main.java
@@ -3,7 +3,7 @@ package br.edu.fatecpg.alejava;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Primeira modificação");
 
 	}
 
